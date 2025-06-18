8e8ffcc5a0aa83460084e787941ed8eac9f27bf6
Quantizr (Jimmy) <9859727+Quantizr@users.noreply.github.com>
2025-06-18 11:01:05 -0700

    fix lock steerRatio

--- selfdrive/locationd/paramsd.py
+++ selfdrive/locationd/paramsd.py
@@ -118,8 +118,7 @@ class VehicleParamsLearner:
           self.kf.predict_and_observe(t, ObservationKind.STEER_RATIO, np.array([[steer_ratio]]))
         else:
           # Observe the current steer_ratio with very low noise to lock the estimate
-          steer_ratio_lock_R = np.atleast_2d(0.01**2)
-          self.kf.predict_and_observe(t, ObservationKind.STEER_RATIO, np.array([[steer_ratio]]), R=steer_ratio_lock_R)
+          self.kf.predict_and_observe(t, ObservationKind.STEER_RATIO, np.array([[steer_ratio]]), R=np.array([[[0.01**2]]]))
 
     elif which == 'liveCalibration':
       self.calibrator.feed_live_calib(msg)
