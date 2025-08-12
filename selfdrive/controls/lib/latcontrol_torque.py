f45f6a12803344aa6450cd5ee6e6fac5906d706e
Quantizr (Jimmy) <9859727+Quantizr@users.noreply.github.com>
2025-08-12 03:39:32 -0700

    odyssey specific changes

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -64,7 +64,9 @@ class LatControlTorque(LatControl):
       pid_log.error = float(torque_from_setpoint - torque_from_measurement)
       ff = self.torque_from_lateral_accel(LatControlInputs(gravity_adjusted_lateral_accel, roll_compensation, CS.vEgo, CS.aEgo), self.torque_params,
                                           gravity_adjusted=True)
-      ff += get_friction(desired_lateral_accel - actual_lateral_accel, lateral_accel_deadzone, FRICTION_THRESHOLD, self.torque_params)
+      friction = get_friction(desired_lateral_accel - actual_lateral_accel, lateral_accel_deadzone, FRICTION_THRESHOLD, self.torque_params)
+      friction /= (1 + abs(gravity_adjusted_lateral_accel)) # less friction with high lataccel - absolutely necessary for highly sigmoidal controllers
+      ff += friction
 
       freeze_integrator = steer_limited_by_safety or CS.steeringPressed or CS.vEgo < 5
       output_torque = self.pid.update(pid_log.error,
