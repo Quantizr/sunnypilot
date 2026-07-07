db8bcf73a1e7e0ce5ca75d7755307af86109d657
Quantizr (Jimmy) <9859727+Quantizr@users.noreply.github.com>
2026-07-06 21:30:49 -0700

    comment out custom launch_openpilot

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -18,7 +18,8 @@ if [ "$MODEL" = "comma tici" ]; then
 fi
 
 # temporary while testing
-export ATHENA_HOST="wss://athena.jimmyfang.com"
-export API_HOST="https://api.jimmyfang.com/"
+#export ATHENA_HOST="wss://athena.jimmyfang.com"
+#export API_HOST="https://api.jimmyfang.com/"
+#export DISABLE_WIDE_ROAD=1
 
 exec ./launch_chffrplus.sh
